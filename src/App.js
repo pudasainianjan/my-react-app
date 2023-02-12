48d8b106b7d664726f5dc3ca28b35d04a689de3a
pudasainianjaan <pudasainianjaan@gmail.com>
2023-02-12 08:05:13 +0545

    set state

--- src/App.js
+++ src/App.js
@@ -29,19 +29,28 @@ import React, { useState, useEffect } from "react";
 // }
 
 function App() {
+  //this is state of app
   const [date, setDate] = useState(new Date());
+  const [myName, setMyName] = useState("Anjan");
 
   useEffect(() => {
     console.log("this component is updated");
   });
+
   useEffect(() => {
+    // setTimeout(() => {
+    //   setMyName("Ram");
+    // }, 5000);
+    // const timerID = setInterval(() => setDate(new Date()), 1000);
     const timerID = setInterval(() => setDate(new Date()), 1000);
   }, []);
+
   useEffect(() => {}, [date]);
   return (
     <div>
-      {/* <h1>Hello, world!</h1> */}
-      <h2>It is {date.toLocaleTimeString()}</h2>
+      <h1>Hello, world! The current time is:</h1>
+      <h2>{date.toLocaleTimeString()}</h2>
+      {/* <h2>My name is {myName}</h2> */}
     </div>
   );
 }
